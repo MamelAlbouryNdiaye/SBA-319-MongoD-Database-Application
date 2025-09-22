35bd220b4f3a093a4d5d3fbc8cc9de7c9c79f63f
Mamel Alboury NDIAYE <Mamel Aly>
2025-09-22 15:51:35 +0200

    updating coursesRoute and adding userRoutes

--- routes/courseRoutes.mjs
+++ routes/courseRoutes.mjs
@@ -5,36 +5,6 @@ import User from "../models/user.mjs"
 const router = express.Router()
 
 
-// GET all users
-router.get("/", async (req, res) => {
-  res.json(await User.find());
-});
-
-// POST create new user
-router.post("/", async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-// PATCH update user role
-router.patch("/:id", async (req, res) => {
-  try {
-    const updated = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    res.json(updated);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-// DELETE user
-router.delete("/:id", async (req, res) => {
-  await User.findByIdAndDelete(req.params.id);
-  res.send("User deleted");
-});
 
 router.get("/seed", async (req, res) => {
   try {
