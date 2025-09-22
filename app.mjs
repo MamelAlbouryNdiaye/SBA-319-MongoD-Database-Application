60f850b5644284f2fe955ea3fd79777ed9864973
Mamel Alboury NDIAYE <Mamel Aly>
2025-09-22 02:46:36 +0200

    adding db connection

--- app.mjs
+++ app.mjs
@@ -1,10 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './db/connection.mjs'
 
 // setup 
+dotenv.config()
 const app = express()
+const port = process.env.PORT || 3225
+
+
+//db connection
+connectDB();
 
 
 
-app.listen(3400, ()=>{
-    console.log(`listening on port 3400`)
+
+app.listen(port, ()=>{
+    console.log(`listening on port  ${port}`)
 })
+
